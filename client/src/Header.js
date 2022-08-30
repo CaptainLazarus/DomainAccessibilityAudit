4cca6572adbce84d6579668f7ab2bede3deb4fdb
CaptainLazarus <adityagudimetla@gmail.com>
2022-08-30 17:38:26 +0530

    Removed DAA from header

--- client/src/Header.js
+++ client/src/Header.js
@@ -6,12 +6,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Header = () => {
   return (
     <header>
-      <p className="bannerTitle">
+      {/* <p className="bannerTitle">
         <a href="https://github.com/MSU-NatSci/DomainAccessibilityAudit">
           Domain Accessibility Audit
         </a>&nbsp;
         <FontAwesomeIcon icon={faUniversalAccess} color="#3e50b4" size="lg"/>
-      </p>
+      </p> */}
     </header>
   );
 };
